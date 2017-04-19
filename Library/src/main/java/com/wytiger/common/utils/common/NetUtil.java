e043d2905c443560d3eefad859d076834f321975
wytiger <igithub19891105>
2017-04-19 09:55:04 +0800

    getIPAddress

--- Library/src/main/java/com/wytiger/common/utils/common/NetUtil.java
+++ Library/src/main/java/com/wytiger/common/utils/common/NetUtil.java
@@ -11,6 +11,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 /**
  * 判断网络状态. 注意：需要相关网络权限
@@ -198,6 +203,39 @@ public class NetUtil {
 		}
 		return false;
 	}
+
+	/**
+	 * 获取ip地址
+	 *
+	 * @return
+	 */
+	public static String getIPAddress() {
+		String hostIp = "";
+		try {
+			Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+			InetAddress ia = null;
+			while (enumeration.hasMoreElements()) {
+				NetworkInterface ni =  enumeration.nextElement();
+				Enumeration<InetAddress> ias = ni.getInetAddresses();
+				while (ias.hasMoreElements()) {
+					ia = ias.nextElement();
+					if (ia instanceof Inet6Address) {
+						continue;// skip ipv6
+					}
+					String ip = ia.getHostAddress();
+					if (!"127.0.0.1".equals(ip)) {
+						hostIp = ia.getHostAddress();
+						break;
+					}
+				}
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		return hostIp;
+	}
+
+
 	/**
 	 * 打开网络设置界面
 	 */
