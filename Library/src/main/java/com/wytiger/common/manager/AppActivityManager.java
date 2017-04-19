9f6ae1def3903c3e9775488d2d37ecf198d8fa23
wytiger <igithub19891105>
2017-04-19 10:00:24 +0800

    退出app方法

--- Library/src/main/java/com/wytiger/common/manager/AppActivityManager.java
+++ Library/src/main/java/com/wytiger/common/manager/AppActivityManager.java
@@ -11,71 +11,72 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class AppActivityManager {
-	private  static List<Activity> activitys = new LinkedList<Activity>();
-	private  static List<Service> services = new LinkedList<Service>();
-	
-	private AppActivityManager() {
-		//不能实例化
-	}
-	
-	public static void addActivity(Activity activity) {
-		activitys.add(activity);
-	}
+    private static List<Activity> activitys = new LinkedList<Activity>();
+    private static List<Service> services = new LinkedList<Service>();
 
-	public static void removeActivity(Activity activity) {
-		activitys.remove(activity);
-	}
+    private AppActivityManager() {
+        //不能实例化
+    }
 
-	public static void addService(Service service) {
-		services.add(service);
-	}
+    public static void addActivity(Activity activity) {
+        activitys.add(activity);
+    }
 
-	public static void removeService(Service service) {
-		services.remove(service);
-	}
+    public static void removeActivity(Activity activity) {
+        activitys.remove(activity);
+    }
 
+    public static void addService(Service service) {
+        services.add(service);
+    }
 
-	private static void closeActivitys() {
-		ListIterator<Activity> iterator = activitys.listIterator();
-		while (iterator.hasNext()) {
-			Activity activity = iterator.next();
-			if (activity != null) {
-				activity.finish();
-			}
-		}
-	}
-
-	private static void closeServices() {
-		ListIterator<Service> iterator = services.listIterator();
-		while (iterator.hasNext()) {
-			Service service = iterator.next();
-			if (service != null) {
-				service.stopSelf();
-			}
-		}
-	}
+    public static void removeService(Service service) {
+        services.remove(service);
+    }
 
 
-	/**
-	 * 关闭应用。关闭所有的Activity与Service
-	 */
-	public static void closeApplication() {
-		closeActivitys();
-		closeServices();
-//		Process.killProcess(Process.myPid());
-	}
-	
-	/**
-	 * 根据包名启动应用
-	 * @param ctx
-	 * @param packageName
-	 */
-	public static void startApp(Context ctx, String packageName) {
-		PackageManager pm = ctx.getPackageManager();
-		Intent it = pm.getLaunchIntentForPackage(packageName);
-		ctx.startActivity(it);
-	}
-	
-	
-	
+    private static void closeActivitys() {
+        ListIterator<Activity> iterator = activitys.listIterator();
+        while (iterator.hasNext()) {
+            Activity activity = iterator.next();
+            if (activity != null) {
+                activity.finish();
+            }
+        }
+    }
+
+    private static void closeServices() {
+        ListIterator<Service> iterator = services.listIterator();
+        while (iterator.hasNext()) {
+            Service service = iterator.next();
+            if (service != null) {
+                service.stopSelf();
+            }
+        }
+    }
+
+
+    /**
+     * 关闭应用。关闭所有的Activity与Service
+     */
+    public static void exitApp() {
+        closeActivitys();
+        closeServices();
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(0);
+    }
+
+    /**
+     * 根据包名启动应用
+     *
+     * @param ctx
+     * @param packageName
+     */
+    public static void startApp(Context ctx, String packageName) {
+        PackageManager pm = ctx.getPackageManager();
+        Intent it = pm.getLaunchIntentForPackage(packageName);
+        ctx.startActivity(it);
+    }
+
+
 }
